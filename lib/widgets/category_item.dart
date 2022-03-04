90b3062879a088dfdc79c818b35604d920110971
Md. Iquebal Mahmud <ikbalmt@gmail.com>
2022-03-04 11:16:52 +0600

    start daily meals

---
+++ lib/widgets/category_item.dart
@@ -0,0 +1,48 @@
+
+import '../screens/category_meals_screen.dart';
+import 'package:flutter/material.dart';
+
+class CategoryItem extends StatelessWidget {
+  final String id;
+  final String title;
+  final Color color;
+
+  CategoryItem(this.id, this.title, this.color);
+
+  void SelectCategory(BuildContext ctx) {
+    Navigator.of(ctx).pushNamed(
+     CategoryMealsScreen.routName, arguments: {
+       'id': id,
+       'title': title,
+     }
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      onTap: () => SelectCategory(context),
+      splashColor: Theme.of(context).primaryColor,
+      borderRadius: BorderRadius.circular(15),
+      child: Container(
+        padding: const EdgeInsets.all(15),
+        child: Text(
+          title,
+          //========= Here video => title, but show error ===================
+          style: Theme.of(context).textTheme.subtitle1,
+        ),
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            colors: [
+              color.withOpacity(0.7),
+              color,
+            ],
+            begin: Alignment.topLeft,
+            end: Alignment.bottomRight,
+          ),
+          borderRadius: BorderRadius.circular(15),
+        ),
+      ),
+    );
+  }
+}
